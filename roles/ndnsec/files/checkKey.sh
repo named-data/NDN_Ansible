fde3b6df209dfaeed0f74fd44dd7a08b7bb9c6f9
John DeHart <jdd@johns-mbp-2.gateway.pace.com>
2016-12-08 16:44:20 -0600

    fix checkKey

--- roles/ndnsec/files/checkKey.sh
+++ roles/ndnsec/files/checkKey.sh
@@ -13,40 +13,11 @@ else
   NDN_NAME=$1
 fi
 
-DEF_NAME=`sudo su - ndnsec -c "ndnsec get-default 2> /dev/null"`
-if [ $? -ne 0 ]
-then
-  echo "failed to get default name"
-  exit 1
-fi
-
-if [ "$NDN_NAME" != "$DEF_NAME" ]
-then
-  echo "given name was not default name"
-  exit 1
-fi
-
 DEF_KEY=`sudo su - ndnsec -c "ndnsec get-default -k 2> /dev/null"`
 if [ $? -ne 0 ]
 then
   echo "failed to get default key"
   exit 1
 fi
-DEF_CERT=`sudo su - ndnsec -c "ndnsec get-default -c 2> /dev/null"`
-if [ $? -ne 0 ]
-then
-  echo "failed to get default cert"
-  exit 1
-fi
 
-#echo "Default Name: $DEF_NAME"
-#echo "Default  Key: $DEF_KEY"
-#echo "Default Cert: $DEF_CERT"
-
-./checkForValidCert.sh $DEF_NAME
-if [ $? -eq 0 ]
-then
-  echo "Found cert with valid date"
-else
-  echo "Did NOT find cert with valid date"
-fi
+exit 0
