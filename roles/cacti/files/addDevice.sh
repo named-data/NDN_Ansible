2658d2d26d1ce81e29dcb058f7bff7b1fde66d69
John DeHart <jdd@johns-mbp-2.seas.wustl.edu>
2017-05-02 10:53:54 -0500

    update cacti maintenance scripts

--- roles/cacti/files/addDevice.sh
+++ roles/cacti/files/addDevice.sh
@@ -3,5 +3,10 @@
 CACTI_DIR=/usr/share/cacti/
 NAME=$1
 HOSTNAME=$2
-php -q $CACTI_DIR/cli/add_device.php --description="$NAME NDN Gateway" --ip=$HOSTNAME --avail=ping --ping_method=tcp --ping_port=22 --ping_retries=1 --timeout=1000 --version=0 --notes="$NAME NDN Testbed Gateway Node does not use SNMP"
+if [ "$NAME" = "PADUA" ]
+then
+  php -q $CACTI_DIR/cli/add_device.php --description="$NAME NDN Gateway" --ip=$HOSTNAME --avail=ping --ping_method=tcp --ping_port=22222 --ping_retries=1 --timeout=1000 --version=0 --notes="$NAME NDN Testbed Gateway Node does not use SNMP"
+else
+  php -q $CACTI_DIR/cli/add_device.php --description="$NAME NDN Gateway" --ip=$HOSTNAME --avail=ping --ping_method=tcp --ping_port=22 --ping_retries=1 --timeout=1000 --version=0 --notes="$NAME NDN Testbed Gateway Node does not use SNMP"
+fi
 
