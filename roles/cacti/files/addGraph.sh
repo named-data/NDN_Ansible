97c1d55cfa34aa42533428a4ac2668dbed331887
John DeHart <jdd@johns-mbp-2.seas.wustl.edu>
2017-05-02 10:53:28 -0500

    update cacti maintenance scripts

--- roles/cacti/files/addGraph.sh
+++ roles/cacti/files/addGraph.sh
@@ -8,6 +8,7 @@ PREFIX=$3
 
 NODE_ID=`$CACTI_DIR/cli/add_graphs.php --list-hosts | grep $NODE | cut -f 1`
 GRAPH_ID=`$CACTI_DIR/cli/add_graphs.php --list-graph-templates | grep "$GRAPH_STRING" | cut -f 1`
-$CACTI_DIR/cli/add_graphs.php --graph-template-id=$GRPAH_ID  --host-id=$NODE_ID --input-fields="prefix=$PREFIX" --graph-type=cg
+$CACTI_DIR/cli/add_graphs.php --graph-template-id=$GRAPH_ID  --host-id=$NODE_ID --input-fields="prefix=$PREFIX" --graph-type=cg
 
 
+exit 0
