8c017e9cb226230cd9a9a451097f6a83fa66fe0b
John DeHart <jdd@Johns-MBP-2.attlocal.net>
2018-03-18 12:13:01 -0500

    add new cert structure where nlsr uses testbed root and site certs

--- roles/ndnsec/files/installCert.sh
+++ roles/ndnsec/files/installCert.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-# ndnsec get-default
-# ndnsec get-default -c
-# ndnsec get-default -k
-#ndnsec-install-cert "http: ... "
 
-sudo su - ndnsec -c "wget $1 -O cert.out"
-sudo su - ndnsec -c "ndnsec-install-cert -f cert.out "
+sudo su - ndnsec -c "ndnsec-cert-install -f /etc/ndn/keys/default.ndncert "
 
-#sudo su - ndnsec -c "ndnsec-install-cert $1 "
-
-sudo su - ndnsec -c "ndnsec-set-default -n $2"
-
-sudo su - ndnsec -c "ndnsec-cert-dump -i $2 >& default.ndncert"
-
-sudo cp ~ndnsec/default.ndncert /etc/ndn/keys
 
 
 
