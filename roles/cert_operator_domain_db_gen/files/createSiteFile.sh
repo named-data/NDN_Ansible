4209c708e07bebd66ebf9388156bc0052c10f4fe
John DeHart <jdd@wustl.edu>
2018-04-17 14:29:12 -0500

    latest updates including TLS port checking

--- roles/cert_operator_domain_db_gen/files/createSiteFile.sh
+++ roles/cert_operator_domain_db_gen/files/createSiteFile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # cat operator file > sitefile
-# echo -n "cert:" >> sitefile
+# echo -n "key:" >> sitefile
 # tr -d '\n' WU.cert >> sitefile
 
 #cert_operator_domain_db_dir/WU.cert				cert_operator_domain_db_dir/cert_operator_domain_db.WU.txt
@@ -13,9 +13,11 @@ DIR=roles/cert_operator_domain_db_gen/files/cert_operator_domain_db_dir
 SITEFILE=${DIR}/cert_operator_domain_db.${SITE}.txt
 CERTFILE=${DIR}/${SITE}.cert
 OPERATORFILE=${DIR}/operator_domain_db.${SITE}.txt
+TLSFILE=${DIR}/${SITE}_TLS.txt
 
 cat ${OPERATORFILE} > $SITEFILE
-echo -n "\"cert\": \"" >> $SITEFILE
+cat ${TLSFILE} >> $SITEFILE
+echo -n "    \"key\": \"" >> $SITEFILE
 cat ${CERTFILE} | tr -d '\n' >> $SITEFILE
 echo  "\"" >> $SITEFILE
 
