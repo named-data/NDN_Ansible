b0a9bbed100a66f6cc843a9a11fce8a691b8e873
John DeHart <jdd@wustl.edu>
2018-08-15 15:50:52 -0500

    latest versions

--- roles/cert_operator_domain_db_gen/files/combineFiles.sh
+++ roles/cert_operator_domain_db_gen/files/combineFiles.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 FILES=`ls roles/cert_operator_domain_db_gen/files/cert_operator_domain_db_dir/cert_operator_domain_db.*.txt`
-first=true
+first=false
 #echo "FILES: $FILES"
+cat roles/cert_operator_domain_db_gen/files/Root.entry
 for f in $FILES
 do
 #  echo "f: $f"
