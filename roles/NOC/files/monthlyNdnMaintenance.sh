24c23ad71fbebe95e4158238f444e778e24584d0
John DeHart <jdd@wustl.edu>
2019-03-29 12:46:36 -0500

    Add missing files

---
+++ roles/NOC/files/monthlyNdnMaintenance.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# This will almost certainly be done via a chronjob.
+# Do we want to stagger this so a lot of nodes are not doing it all at the same time?
+
+# Once a month, perform the following maintenance:
+# reboot?
+# nfd restart? (At least this, if not a full reboot)
+
+# Once a week, perform the following maintenance:
+# apt-get autoremove; apt-get update; apt-get upgrade; apt-get autoremove
+
+
+LOGFILE="/tmp/monthlyNdnMaintenance.log"
+date >& $LOGFILE
+/usr/sbin/service nfd restart >> $LOGFILE
+
+
---
+++ roles/NOC/files/weeklyNdnMaintenance.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# This will almost certainly be done via a cronjob.
+# Do we want to stagger this so a lot of nodes are not doing it all at the same time?
+
+# Once a month, perform the following maintenance:
+# reboot?
+# nfd restart? (At least this, if not a full reboot)
+
+# Once a week, perform the following maintenance:
+# apt-get autoremove; apt-get update; apt-get upgrade; apt-get autoremove
+
+LOGFILE="/tmp/weeklyNdnMaintenance.log"
+
+date >& $LOGFILE
+echo "------ df------" >> $LOGFILE
+df -h >> $LOGFILE
+echo "------ apt-get -y autoremove ------" >> $LOGFILE
+apt-get -y autoremove  >> $LOGFILE
+echo "------ apt-get update ------" >> $LOGFILE
+apt-get update >> $LOGFILE
+echo "------ apt-get -y upgrade ------" >> $LOGFILE
+apt-get -y upgrade >> $LOGFILE
+echo "------ apt-get -y autoremove ------" >> $LOGFILE
+apt-get -y autoremove >> $LOGFILE
+echo "------ df------" >> $LOGFILE
+df -h >> $LOGFILE
+echo "------ done------" >> $LOGFILE
